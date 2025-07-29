a2f5709c70d4f2dd1c6d24fa9a3a2925dd2e8453
Nikoo <nikoomoallemi@gmail.com>
2025-07-29 13:54:52 +0330

    foundation complete

--- Mood-Weather.sh
+++ Mood-Weather.sh
@@ -2,39 +2,37 @@
 
 
 #---------Config----------
-API-KEY = "347ca835cff44547af150547252607"
+API_KEY="347ca835cff44547af150547252607"
 CITY="ESFAHAN"
-QUOTE_FILE= "quote.txt"
-
 
 #----------Fetch----------
-WEATHER_JSON = $(curl -s "http://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$CITY")
-CONDITION = $(echo "$WEATHER_JSON" |  jq -r 'current.condition.text')
-TEMPR = $(echo "WEATHER_JSON" | jq -r '.current.temp_c')
+WEATHER_JSON=$(curl -s "http://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$CITY")
+CONDITION=$(echo "$WEATHER_JSON" |  jq -r '.current.condition.text')
+TEMPR=$(echo "$WEATHER_JSON" | jq -r '.current.temp_c')
 
 #---------output weather info---------
-echo "📍 $CITY"
-echo "🌡️ Temperature: $TEMP°C"
-echo "🌤️ Condition: $CONDITION"
+echo "📍$CITY"
+echo "🌡️Temperature: $TEMPR °C"
+echo "🌤️Condition: $CONDITION"
 
 #------quotes based on weather--------
-QUOTE_FILE =""
+QUOTE_FILE=""
 
  if echo "$CONDITION" | grep -iqE "Rain|Fog|Cloud"; then
-	QUOTE_FILE = "gloomy.txt"
- elif echo "$CONDITION" | grep -iqE "Sun | Clear"; then
-	QUOTE_FILE = "sunny.txt"
+	QUOTE_FILE="./gloomy.txt"
+ elif echo "$CONDITION" | grep -iqE "Sun|Clear"; then
+	QUOTE_FILE="sunny.txt"
  elif echo "$CONDITION" | grep -iqE "Snow|Cold"; then
-	QUOTE_FILE = "Cozy.txt"
+	QUOTE_FILE="./Cozy.txt"
  else
-	QUOTE_FILE = "gloomy.txt"
+	QUOTE_FILE="./gloomy.txt"
 fi
+
 #------checking and handeling the txt files missing
 if [[ -s "$QUOTE_FILE" ]]; then
-	QUOTE = $(shuf -n 1 "$QUOTE_FILE")
+	QUOTE=$(shuf -n 1 "$QUOTE_FILE")
 else
-	QUOTE = "Just Keep Swimming."
+	QUOTE="Just Keep Swimming."
 fi
 #------- Quote show------------------
   echo "🧾 $QUOTE"
-fi
